bf7a0b0b175987b6e4736ce1297ab5810d963e54
Petar <petarsamardzija12@gmail.com>
2020-01-16 09:38:37 +0100

    Settings design update

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 
 
 
 import { auth } from '../config';
 
-const Settings = ({ navigation }) => {
+export default Settings = ({ navigation }) => {
 
   handleSignOut = () => {
     auth
@@ -15,18 +15,64 @@ const Settings = ({ navigation }) => {
   };
 
     return (
-        <View>
-            <Text>Settings</Text>
-            <Button 
+        <View style={styles.main}>
+            <View style={styles.container}>
+                <View>
+                    <View style={styles.input}>
+                        <Text>From:</Text>
+                        <TextInput style={styles.textInput}/>
+                    </View>
+                    <View style={styles.input}>
+                        <Text>To:</Text>
+                        <TextInput style={styles.textInput}/>
+                    </View>
+                </View>
+                <View style={styles.input}>
+                    <Text>To Email:</Text>
+                    <TextInput style={styles.textInput}/>
+                </View>
+            </View>
+            <View style={styles.footer}>
+              <Button 
                 title="Send your hours"
-                onPress={()  => console.log()}
-            />
-            <Button 
-                title="Sign Out"
-                onPress={handleSignOut}
-            />
+                onPress={()  => alert('Your hours have been sent!')}
+                />
+                <Button 
+                    title="Sign Out"
+                    onPress={handleSignOut}
+                />   
+            </View>   
         </View>
     )
 }
 
-export default Settings;
+const styles = StyleSheet.create({
+    main:{
+        flex:1,
+        backgroundColor: 'yellow',
+        justifyContent: 'space-between',
+        paddingVertical: 35
+    },
+    input:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+
+    },
+    container: {
+      alignItems: 'center'
+    },
+    footer: {
+        justifyContent: 'flex-end',
+      },
+    textInput: {
+      height: 40,
+      width: '80%',
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginTop: 8,
+      paddingLeft:10,
+    },
+    text: {
+      fontSize: 30
+    }
+  });
